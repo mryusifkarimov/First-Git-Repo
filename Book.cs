3eb672d9ac1733042a17c2ad9449dee573926b3c
Yusif Karimov <46921214+mryusifkarimov@users.noreply.github.com>
2026-04-28 22:20:06 +0400

    Note propery-sin Book clasına əlavə etdik

--- Book.cs
+++ Book.cs
@@ -4,12 +4,14 @@
     {
         public string Name { get; set; }
         public int PageNo { get; set; }
-        public Book(string Name, int PageNo)
+        public string Note { get; set; }
+        public Book(string Name, int PageNo, string note
         {
             this.Name = Name;
             this.PageNo = PageNo;
+            this.Note = note;
         }
-        public override string ToString() => $"Name:{Name}, PageNo:{PageNo}";
+        public override string ToString() => $"Name:{Name}, PageNo:{PageNo, Note:{Note}";
     }
 
 
